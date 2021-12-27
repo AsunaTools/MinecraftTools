bceb60db3a13c10285f87e41f7f0974a9d8c6708
fiwi1265 <fiwi1265@asuna.tools>
2021-12-27 20:30:09 +0100

    Update code

--- App.cs
+++ App.cs
@@ -2,31 +2,32 @@
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace MinecraftTools {
     class App {
         private static void Main() {
             Console.Clear();
             Console.WriteLine("==========================================================================");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("       d8888                                     888b     d888  .d8888b.  ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("      d88888                                     8888b   d8888 d88P  Y88b ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("     d88P888                                     88888b.d88888 888    888 ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("    d88P 888 .d8888b  888  888 88888b.   8888b.  888Y88888P888 888        ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("   d88P  888 88K      888  888 888 " + '\u0022' + "88b     " + '\u0022' +  "88b 888 Y888P 888 888        ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("  d88P   888 " + '\u0022' + "Y8888b. 888  888 888  888 .d888888 888  Y8P  888 888    888 ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine(" d8888888888      X88 Y88b 888 888  888 888  888 888   " + '\u0022' + "   888 Y88b  d88P ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("d88P     888  88888P   " + '\u0022' + "Y88888 888  888 " + '\u0022' + "Y888888 888       888  " + '\u0022' + "Y8888P" + '\u0022' + "  ");
-            System.Threading.Thread.Sleep(100);
+            Thread.Sleep(100);
             Console.WriteLine("==========================================================================");
-            System.Threading.Thread.Sleep(1000);
+            Thread.Sleep(1000);
 
             if (Directory.Exists(MinecraftDir())) {
                 var showMenu = true;
@@ -37,7 +38,7 @@ namespace MinecraftTools {
                 Console.WriteLine("===================================");
                 Console.WriteLine();
                 Console.WriteLine("¡Hecho! Gracias por usar AsunaTools una vez mas,");
-                Console.WriteLine("Escrito por Asuna y Ekardo.");
+                Console.WriteLine("Escrito por Asuna y Ekardo. (Reescrito en C# por Asuna)");
                 Console.WriteLine();
                 Console.WriteLine("Presione cualquier tecla para salir.");
                 Console.ReadLine();
@@ -88,24 +89,50 @@ namespace MinecraftTools {
                     return true;
                 case "2":
                     Console.Clear();
-                    Console.WriteLine("No");
+                    Console.WriteLine("Work In Progress");
                     return true;
                 case "3":
                     Console.Clear();
-                    Console.WriteLine("No");
+                    Console.WriteLine("Work In Progress");
                     return true;
                 default:
                     return false;
             }
         }
         private static void InstallMods() {
+            Console.WriteLine("===================================");
+            Console.WriteLine("          Minecraft Tools          ");
+            Console.WriteLine("===================================");
+            Console.WriteLine();
             var modsDir = MinecraftDir() + "/mods";
             if (!Directory.Exists(modsDir)) {
                 Directory.CreateDirectory(modsDir);
             }
-            using var client = new WebClient();
-            client.DownloadFile("https://asuna.tools/data/downloads/asunamc/mods.zip",@modsDir);
+            Console.Write("Clear mods folder: ");
+            DirectoryInfo di = new DirectoryInfo(modsDir);
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete(); 
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true); 
+            }
+            Console.WriteLine("Done");
+            Console.Write("Download mods.zip: ");
+            WebClient Client = new WebClient ();
+            Client.DownloadFile(new Uri("https://static.asuna.tools/downloads/asunamc/mods.zip"), @modsDir + "/mods.zip");
+            Console.WriteLine("Done");
+            Console.Write("Extract mods: ");
+            System.IO.Compression.ZipFile.ExtractToDirectory(@modsDir + "/mods.zip", @modsDir);
+            Console.WriteLine("Done");
+            Console.Write("Delete Temp: ");
+            if(File.Exists(@modsDir + "/mods.zip"))
+            {
+                File.Delete(@modsDir + "/mods.zip");
+            }
+            Console.WriteLine("Done");
+            Thread.Sleep(300);
         }
-        
     }
 }
